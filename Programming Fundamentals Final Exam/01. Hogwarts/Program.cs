16af73153477af432c0cf556476a983b6b7f312a
vgp5sas <venelin.pavlov@gmail.com>
2022-12-04 20:12:18 +0200

    Add project files.

---
+++ Programming Fundamentals Final Exam/01. Hogwarts/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+
+
+namespace _01._Hogwarts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringInput = Console.ReadLine();
+            string commandInput = Console.ReadLine();
+
+            while (commandInput != "Abracadabra")
+            {
+                string[] commands = commandInput.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                switch (commands[0])
+                {
+                    case "Abjuration":
+                        stringInput = stringInput.ToUpper();
+                        Console.WriteLine(stringInput);
+                        break;
+                    case "Necromancy":
+                        stringInput = stringInput.ToLower();
+                        Console.WriteLine(stringInput);
+                        break;
+                    case "Illusion":
+                        int position = int.Parse(commands[1]);
+                        string replacingLetter = commands[2];
+                        if (position >= 0 && position <= stringInput.Length - 1)
+                        {
+                            stringInput = stringInput.Remove(position, 1);
+                            stringInput = stringInput.Insert(position, replacingLetter);
+                            Console.WriteLine("Done!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("The spell was too weak.");
+                        }
+                        break;
+                    case "Divination":
+                        string substring1 = commands[1];
+                        string substring2 = commands[2];
+                        if (stringInput.Contains(substring1))
+                        {
+                            stringInput = stringInput.Replace(substring1, substring2);
+                            Console.WriteLine(stringInput);
+                        }
+                        break;
+                    case "Alteration":
+                        string substring3 = commands[1];
+                        if (stringInput.Contains(substring3))
+                        {
+                            stringInput = stringInput.Replace(substring3, "");
+                            Console.WriteLine(stringInput);
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("The spell did not work!");
+                        break;
+                }
+                commandInput = Console.ReadLine();
+            }
+        }
+    }
+}
+
---
+++ Programming Fundamentals Final Exam/02. Message Translator/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _02._Message_Translator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberInputs = int.Parse(Console.ReadLine());
+            Regex specimen = new Regex(@"(!)(?<command>[A-Z][a-z]{2,})(\1).([[])(?<message>[A-Za-z]{8,})([]])");
+
+            for (int i = 0; i < numberInputs; i++)
+            {
+                string inputMessage = Console.ReadLine();
+                Match valid = specimen.Match(inputMessage);
+
+                if (valid.Success)
+                {
+                    string line = valid.Groups["message"].Value;
+
+                    Console.Write($"{valid.Groups["command"]}: ");
+
+                    foreach (var characters in line)
+                    {
+                        int n = characters;
+
+                        Console.Write($"{n} ");
+                    }
+                    Console.WriteLine();
+                }
+
+                else
+                {
+                    Console.WriteLine("The message is invalid");
+                }
+
+            }
+        }
+    }
+}
+
